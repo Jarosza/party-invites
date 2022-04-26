451d3601bc69cb0bd6348acf57bc04744b5e6d21
Anna Jarosz <annajarosz1310@gmail.com>
2022-04-26 14:09:27 +0200

    First commit with project source code

---
+++ src/pl/jaknauczycsieprogramowania/Main.java
@@ -0,0 +1,39 @@
+package pl.jaknauczycsieprogramowania;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean shouldContinue = true;
+
+        Party party = new Party();
+
+
+        while (shouldContinue) {
+            System.out.println("Wybierz opcję");
+            System.out.println("1. Wyświetl gości");
+            System.out.println("2. Dodaj gościa");
+            System.out.println("3. Wyświetl potrawy");
+            System.out.println("4. Znajdź po numerze telefonu");
+            System.out.println("5. Wyjście");
+
+            int userChoice = scanner.nextInt(); // utworz zmienna PRZED pętlą
+
+
+            switch (userChoice) {
+                case 1 -> party.displayGuests();
+                case 2 -> party.addNewGuest();
+                case 3 -> System.out.println("wybrano 3");
+                case 4 -> party.displayGuestByPhoneNumber();
+                case 5 -> shouldContinue = false;
+
+            }
+        }
+    }
+}
+
+
+
+
+
